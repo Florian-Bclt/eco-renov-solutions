e35904ac8ada805876a758ce3de774de18e51d6b
Florian Bouclet <bouclet.florian@hotmail.com>
2025-03-12 22:22:27 +0100

    fix params delete

--- src/app/api/submission/[id]/route.ts
+++ src/app/api/submission/[id]/route.ts
@@ -3,7 +3,11 @@ import prisma from "@/lib/prisma";
 import { authOptions } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request, context: { params: { id: string } }) {
+type Context = {
+  params: { id: string };
+};
+
+export async function DELETE(req: Request, { params }: Context ) {
   try {
     // Vérifie si l'utilisateur est authentifié et est admin
     const session = await getServerSession(authOptions);
@@ -12,7 +16,7 @@ export async function DELETE(req: Request, context: { params: { id: string } }) 
     }
 
     // Vérifie que l'ID est valide
-    const submissionId = parseInt( context.params.id, 10);
+    const submissionId = parseInt(params.id, 10);
     if (isNaN(submissionId)) {
       return NextResponse.json({ error: 'ID invalide' }, { status: 400});
     }
